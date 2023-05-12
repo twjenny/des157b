50864d03fee8c410155c00a97e397bf13fe929c1
Jennifer Kim <jkjkim0217@gmail.com>
2023-05-11 20:59:51 -0700

    paper prototype added

---
+++ libraryLiberty/script.js
@@ -0,0 +1,24 @@
+(function(){
+    'use strict';
+
+    AOS.init();
+
+    // var colorGradient = new Granim({
+    //     element: '#logo-canvas',
+    //     direction: 'left-right',
+    //     states : {
+    //         "default-state": {
+    //             gradients: [
+    //                 ['#EB3349', '#F45C43'],
+    //                 ['#FF8008', '#FFC837'],
+    //                 ['#4CB8C4', '#3CD3AD'],
+    //                 ['#24C6DC', '#514A9D'],
+    //                 ['#FF512F', '#DD2476'],
+    //                 ['#DA22FF', '#9733EE']
+    //             ],
+    //             transitionSpeed: 2000
+    //         }
+    //     }
+    // });
+
+})();
