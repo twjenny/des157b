f0df79073afd4b855c23c6972ff64784ef02eeb2
Jennifer Kim <jkjkim0217@gmail.com>
2023-05-14 17:01:25 -0700

    need help on making JS show on the screen

--- libraryLiberty/script.js
+++ libraryLiberty/script.js
@@ -3,22 +3,41 @@
 
     AOS.init();
 
-    // var colorGradient = new Granim({
-    //     element: '#logo-canvas',
-    //     direction: 'left-right',
-    //     states : {
-    //         "default-state": {
-    //             gradients: [
-    //                 ['#EB3349', '#F45C43'],
-    //                 ['#FF8008', '#FFC837'],
-    //                 ['#4CB8C4', '#3CD3AD'],
-    //                 ['#24C6DC', '#514A9D'],
-    //                 ['#FF512F', '#DD2476'],
-    //                 ['#DA22FF', '#9733EE']
-    //             ],
-    //             transitionSpeed: 2000
-    //         }
-    //     }
-    // });
+    Parse.initialize("D1pu9y1JiK7RycCuQRnleUAbt8uVMmbqDrxh5wRZ","Ewm5ZDgCdBS8cvsIdule1IS0VdsphMUR2Vv0XYUA");
+    Parse.serverURL = 'https://parseapi.back4app.com/'
 
-})();
+    const showPls = document.querySelector("main ol");    
+    async function displayQuestions() {
+    const allquestions = Parse.Object.extend('Questionnaire');
+    const query = new Parse.Query(allquestions);
+    try {
+        const results = await query.ascending('questions').find();
+        console.log(results);
+
+        results.forEach( function(eachQuestion){
+            const id = eachQuestion.id;
+            const ans1 = eachQuestion.get('answer1');
+            const ans2 = eachQuestion.get('answer2');
+            const ans3 = eachQuestion.get('answer3');
+
+            const theListItem = document.createElement("li");
+        theListItem.setAttribute("id", `r-${id}`);
+        theListItem.innerHTML = `
+        <div class="quest">${question}</div>
+        <div class="ans">
+            ${ans1}
+            ${ans2}
+            ${ans3}
+        </div>`;
+        
+        showPls.append(theListItem);
+        });
+    }
+    catch{
+        // console.error('Error while fetching questions', error);
+    };
+    }
+    
+    displayQuestions();
+
+})()
