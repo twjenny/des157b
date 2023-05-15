a75071487f38ac4fd56d89bea0ca2798c0203627
Jennifer Kim <jkjkim0217@gmail.com>
2023-05-14 23:59:29 -0700

    js now showing on screen. Need to work on granim

--- libraryLiberty/script.js
+++ libraryLiberty/script.js
@@ -6,16 +6,18 @@
     Parse.initialize("D1pu9y1JiK7RycCuQRnleUAbt8uVMmbqDrxh5wRZ","Ewm5ZDgCdBS8cvsIdule1IS0VdsphMUR2Vv0XYUA");
     Parse.serverURL = 'https://parseapi.back4app.com/'
 
-    const showPls = document.querySelector("main ol");    
+    const showPls = document.querySelector("section ol");  
+
     async function displayQuestions() {
-    const allquestions = Parse.Object.extend('Questionnaire');
-    const query = new Parse.Query(allquestions);
+    const allQuestions = Parse.Object.extend('Questionnaire');
+    const query = new Parse.Query(allQuestions);
     try {
         const results = await query.ascending('questions').find();
-        console.log(results);
+        // console.log(results);
 
         results.forEach( function(eachQuestion){
             const id = eachQuestion.id;
+            const que = eachQuestion.get('question');
             const ans1 = eachQuestion.get('answer1');
             const ans2 = eachQuestion.get('answer2');
             const ans3 = eachQuestion.get('answer3');
@@ -23,13 +25,15 @@
             const theListItem = document.createElement("li");
         theListItem.setAttribute("id", `r-${id}`);
         theListItem.innerHTML = `
-        <div class="quest">${question}</div>
-        <div class="ans">
-            ${ans1}
-            ${ans2}
-            ${ans3}
-        </div>`;
-        
+            <div class="quest">${que}</div>
+            <div class="answer">
+            <ul>
+                <li>${ans1}</li>
+                <li>${ans2}</li>
+                <li>${ans3}</li>
+            </ul>
+            </div>`;
+            
         showPls.append(theListItem);
         });
     }
